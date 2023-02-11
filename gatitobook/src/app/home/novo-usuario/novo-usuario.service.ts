c6bb5742d30f173fc4070cb618217a7fab678ce3
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-02-10 23:47:54 -0300

    feat: Criado componentes de exibição dos animais e de informação. Criado services de usuario e configurado environment.

--- gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -1,6 +1,9 @@
+import { environment } from './../../../environments/environment';
 import { NovoUsuario } from './novo-usuario';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +13,10 @@ export class NovoUsuarioService {
   constructor(private httpCLient: HttpClient) { }
 
   cadastraNovoUsuario(novoUsuario: NovoUsuario) {
-    return this.httpCLient.post('http://localhost:3000/user/signup', novoUsuario);
+    return this.httpCLient.post(`${API}/user/signup`, novoUsuario);
   }
 
   verificaUsuarioExistente(nomeUsuario: string) {
-    return this.httpCLient.get(`http://localhost:3000/user/exists/${nomeUsuario}`);
+    return this.httpCLient.get(`${API}/user/exists/${nomeUsuario}`);
   }
 }
